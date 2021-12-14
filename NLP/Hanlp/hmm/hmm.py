2f84d46c3b66e3dbbde3b5526c9bba9b33fac8b6
DancingOnAir <taotong1984@hotmail.com>
2021-12-14 23:31:41 +0800

    feat(hmm): implemented hidden markov model strcuture

---
+++ NLP/Hanlp/hmm/hmm.py
@@ -0,0 +1,16 @@
+
+
+states = ("Healthy", "Fever")
+start_probability = {"Healthy": 0.6, "Fever": 0.4}
+transition_probability = {
+    "Healthy": {"Healthy": 0.7, "Fever": 0.3},
+    "Fever": {"Healthy": 0.4, "Fever": 0.6}
+}
+emission_probability = {
+    "Healthy": {"normal": 0.5, "cold": 0.4, "dizzy": 0.1},
+    "Fever": {"normal": 0.1, "cold": 0.3, "dizzy": 0.6}
+}
+observations = ("normal", "cold", "dizzy")
+
+
+def 
