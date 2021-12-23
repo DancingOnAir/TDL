38714d93b5f804c853e7469376bfa02ecd77dad3
DancingOnAir <taotong1984@hotmail.com>
2021-12-23 22:34:43 +0800

    feat(nerual_network_training): implmented cross entropy error batch for test result is actual value function

--- NLP/DeepLearningFromScratch/neural_network_training/mean_squared_error.py
+++ NLP/DeepLearningFromScratch/neural_network_training/mean_squared_error.py
@@ -14,7 +14,7 @@ def cross_entropy_error(y, t):
 
 
 # for one-hot
-def cross_entropy_error_batch(y, t):
+def cross_entropy_error_batch_one_hot(y, t):
     if y.ndim == 1:
         y = y.reshape(1, y.size)
         t = t.reshape(1, t.size)
@@ -22,6 +22,17 @@ def cross_entropy_error_batch(y, t):
     batch_size = y.shape[0]
     delta = 1e-7
     return -np.sum(t * np.log(y + delta)) / batch_size
+
+
+# for non one-hot, actual value
+def cross_entropy_error_batch(y, t):
+    if y.ndim == 1:
+        y = y.reshape(1, y.size)
+        t = t.reshape(1, t.size)
+
+    batch_size = y.shape[0]
+    delta = 1e-7
+    return -np.sum(np.log(y[np.arange(batch_size), t] + delta)) / batch_size
 
 
 def test_lost_function():
