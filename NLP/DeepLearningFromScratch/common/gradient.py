74c70431b96392d32294690cd9cafe52728fa2e2
DancingOnAir <taotong1984@hotmail.com>
2021-12-27 21:37:46 +0800

    feat(common/gradient): implmented numerical gradient function

---
+++ NLP/DeepLearningFromScratch/common/gradient.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def numerical_gradient(f, x):
+    h = 1e-5
+    grad = np.zeros_like(x)
+    for i in range(x.size):
+        tmp_x = x[i]
+        x[i] = tmp_x + h
+        fhx1 = f(x)
+
+        x[i] = tmp_x - h
+        fhx2 = f(x)
+
+        grad[i] = (fhx1 - fhx2) / (2 * h)
+        x[i] = tmp_x
+
+    return grad
