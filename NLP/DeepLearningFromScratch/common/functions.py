7421389689d4613df09d899808dba31e4d499075
Tao Tong <ttong@soundhound.com>
2021-12-28 17:03:33 +0800

    feat(common/functions): implemented sigmoid function

--- NLP/DeepLearningFromScratch/common/functions.py
+++ NLP/DeepLearningFromScratch/common/functions.py
@@ -18,3 +18,7 @@ def cross_entropy_error(y, t):
     batch_size = y.shape[0]
     delta = 1e-7
     return -np.sum(t * np.log(y + delta)) / batch_size
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
