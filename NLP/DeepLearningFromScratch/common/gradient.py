a0cd14cf94b8a8f4182de3b58acb8086d4e897a3
Tao Tong <ttong@soundhound.com>
2021-12-28 11:33:03 +0800

    feat(neural_network): imporved gradient function

--- NLP/DeepLearningFromScratch/common/gradient.py
+++ NLP/DeepLearningFromScratch/common/gradient.py
@@ -4,10 +4,12 @@ import numpy as np
 def numerical_gradient(f, x):
     h = 1e-4
     grad = np.zeros_like(x)
-    for i in range(x.size):
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        i = it.multi_index
         tmp_x = x[i]
         x[i] = tmp_x + h
-
         fhx1 = f(x)
 
         x[i] = tmp_x - h
@@ -15,5 +17,6 @@ def numerical_gradient(f, x):
 
         grad[i] = (fhx1 - fhx2) / (2 * h)
         x[i] = tmp_x
+        it.iternext()
 
     return grad
