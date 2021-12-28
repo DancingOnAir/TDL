111c4e35bfed5c557eddf59b0a942343df6b7f0c
Tao Tong <ttong@soundhound.com>
2021-12-28 17:24:27 +0800

    feat(two_layer_net): implemented two layer net

---
+++ NLP/DeepLearningFromScratch/neural_network_training/two_layer_net.py
@@ -0,0 +1,74 @@
+import sys
+import os
+sys.path.append(os.pardir)
+
+
+from common.functions import softmax, cross_entropy_error, sigmoid
+from common.gradient import numerical_gradient
+import numpy as np
+
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = dict()
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = weight_init_std * np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = weight_init_std * np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda w: self.loss(x, t)
+        grads = dict()
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
+
+def test_two_layer_net():
+    net = TwoLayerNet(784, 100, 10)
+    x = np.random.rand(100, 784)
+    t = np.random.rand(100, 10)
+    print('x')
+    print(x)
+    print('-' * 20)
+    print('t')
+    print(t)
+    print('-' * 20)
+
+    grads = net.numerical_gradient(x, t)
+    print(grads['W1'].shape)
+    print(grads['b1'].shape)
+    print(grads['W2'].shape)
+    print(grads['b2'].shape)
+
+
+if __name__ == '__main__':
+    test_two_layer_net()
+
+
+
