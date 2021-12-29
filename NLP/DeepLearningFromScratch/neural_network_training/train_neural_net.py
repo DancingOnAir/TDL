0ee9c77c92ed9297618aa3360b0ee52f626f43a0
DancingOnAir <taotong1984@hotmail.com>
2021-12-29 22:19:34 +0800

    feat(simplez_network): implmented calculating accuracy of train & test

--- NLP/DeepLearningFromScratch/neural_network_training/train_neural_net.py
+++ NLP/DeepLearningFromScratch/neural_network_training/train_neural_net.py
@@ -10,11 +10,15 @@ from two_layer_net import TwoLayerNet
 
 (train_x, train_t), (test_x, test_t) = load_mnist(normalize=True, flatten=True, one_hot_label=True)
 train_loss_list = list()
+train_acc_list = list()
+test_acc_list = list()
 
-iters_num = 100
+iters_num = 10000
 batch_size = 100
 train_size = train_x.shape[0]
 learning_rate = 0.1
+iter_per_epoch = max(train_size / batch_size, 1)
+print(iter_per_epoch)
 
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
@@ -30,6 +34,14 @@ for i in range(iters_num):
 
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+
+    # if i % iter_per_epoch == 0:
+    train_acc = network.accuracy(x_batch, t_batch)
+    test_acc = network.accuracy(test_x, test_t)
+    train_acc_list.append(train_acc)
+    test_acc_list.append(test_acc)
+    print('train acc, test acc | ' + str(train_acc) + ", " + str(test_acc))
+
 
 print('draw plot')
 x = list(range(1, iters_num+1))
