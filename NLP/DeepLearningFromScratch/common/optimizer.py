b6f24963f9ab490bda36bb0a9afd9eb4cdcea7c0
DancingOnAir <taotong1984@hotmail.com>
2022-01-03 10:52:43 +0800

    feat(optimizer): implmented Momentum

--- NLP/DeepLearningFromScratch/common/optimizer.py
+++ NLP/DeepLearningFromScratch/common/optimizer.py
@@ -10,5 +10,19 @@ class SGD:
             params[key] -= self.lr * grads[key]
 
 
+class Momentum:
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
 
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = dict()
+            for k, val in params.item():
+                self.v[k] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
+            params[key] += self.v[key]
 
