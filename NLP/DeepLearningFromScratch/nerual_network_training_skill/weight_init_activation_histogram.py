7f9713c4cbc1cd1675eaffe12fe0c0a57a31e234
DancingOnAir <taotong1984@hotmail.com>
2022-01-06 09:30:50 +0800

    feat(weight_init_activation_histogram): implemented tanh comparsion

--- NLP/DeepLearningFromScratch/nerual_network_training_skill/weight_init_activation_histogram.py
+++ NLP/DeepLearningFromScratch/nerual_network_training_skill/weight_init_activation_histogram.py
@@ -10,6 +10,10 @@ def relu(x):
     return np.maximum(0, x)
 
 
+def tanh(x):
+    return np.tanh(x)
+
+
 x = np.random.randn(1000, 100)
 node_num = 100
 hidden_layer_size = 5
@@ -19,23 +23,24 @@ activations = dict()
 for i in range(hidden_layer_size):
     if i != 0:
         x = activations[i - 1]
-
-    # w = np.random.randn(node_num, node_num) * 1
+    # std = 1
+    w = np.random.randn(node_num, node_num) * 1
     # std = 0.01
     # w = np.random.randn(node_num, node_num) * 0.01
     # Xavier
     # w = np.random.randn(node_num, node_num) / np.sqrt(node_num)
     # He
-    w = np.random.randn(node_num, node_num) * np.sqrt(2) / np.sqrt(node_num)
+    # w = np.random.randn(node_num, node_num) * np.sqrt(2) / np.sqrt(node_num)
 
     z = np.dot(x, w)
     # a = sigmoid(z)
-    a = relu(z)
+    a = tanh(x)
+    # a = relu(z)
     activations[i] = a
 
 
 for i, a in activations.items():
     plt.subplot(1, len(activations), i+1)
     plt.title(str(i + 1) + " - layer")
-    plt.hist(a.flatten(), 30, range=(0, 1))
+    plt.hist(a.flatten(), 30, range=(-0.5, 0.5))
 plt.show()
