3f44c67feaba595145acfb8d63164cd0d316d8ec
DancingOnAir <taotong1984@hotmail.com>
2022-01-06 17:03:20 +0800

    feat(layers): implemented AddLyer, MulLayer, Sigmoid, Relu

---
+++ NLP/DeepLearningFromScratch/common/layers.py
@@ -0,0 +1,69 @@
+
+import numpy as np
+
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+        pass
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+
+        out = x * y
+        return out
+
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+
+        return dx, dy
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+        pass
+
+    def forward(self, x):
+        self.out = 1 / (1 + np.exp(-x))
+
+        return self.out
+
+    def backward(self, dout):
+        dx = dout * self.out * (1 - self.out)
+        return dx
+
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+        return dx
---
+++ NLP/DeepLearningFromScratch/common/multi_layer_net_extend.py
@@ -0,0 +1,6 @@
+# import os
+# import sys
+# sys.path.append(os.pardir)
+#
+#
+# from common.
