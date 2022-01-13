e685d3e7e819ad31ec44fb17b03be26f44c01c06
DancingOnAir <taotong1984@hotmail.com>
2022-01-13 16:54:06 +0800

    feat(util/im2col); implemented img to col function

--- NLP/DeepLearningFromScratch/common/util.py
+++ NLP/DeepLearningFromScratch/common/util.py
@@ -7,11 +7,18 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
     out_w = 1 + int(W + 2 * pad - filter_w) // stride
 
     img = np.pad(input_data, [(0, 0), (0, 0), (pad, pad), (pad, pad)], 'constant')
-    # 这里6个参数的意义
     col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
 
+    for y in range(filter_h):
+        y_max = y + out_h * stride
+        for x in range(filter_w):
+            x_max = x + out_w * stride
+            # https://blog.csdn.net/cjbct/article/details/100749205
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+    # https://zhuanlan.zhihu.com/p/72381219
+    # reshape default order = 'C',
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N * out_h * out_w, -1)
     return col
-    pass
 
 
 def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
