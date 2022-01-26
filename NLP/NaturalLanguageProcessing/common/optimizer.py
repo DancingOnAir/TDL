9f0b2d9aa550d818eabd5692b275bf4e51b26b10
DancingOnAir <taotong1984@hotmail.com>
2022-01-26 22:59:07 +0800

    feat(optimizer): implemented Momentum method

--- NLP/NaturalLanguageProcessing/common/optimizer.py
+++ NLP/NaturalLanguageProcessing/common/optimizer.py
@@ -8,3 +8,20 @@ class SGD:
     def update(self, params, grads):
         for key in params.keys():
             params[key] -= self.lr * grads[key]
+
+
+class Momentum:
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = dict()
+            for k, val in params.items():
+                self.v[k] = np.zeros_like(val)
+
+        for k in params.keys():
+            self.v[k] = self.momentum * self.v[k] - self.lr * grads[k]
+            params[k] += self.v[k]
