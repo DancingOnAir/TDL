6c9650853146acdb743552725b5094b8070567d1
DancingOnAir <taotong1984@hotmail.com>
2022-01-26 21:52:26 +0800

    feat(optimizer): created optimizer and implemented SGD

---
+++ NLP/NaturalLanguageProcessing/word_distributional_representation/train_simple_cbow.py
@@ -0,0 +1,15 @@
+import sys
+import os
+sys.path.append(os.pardir)
+
+
+from common.trainer import Trainer
+from simple_cbow import SimpleCBOW
+
+
+def train():
+    pass
+
+
+if __name__ == '__main__':
+    train()
