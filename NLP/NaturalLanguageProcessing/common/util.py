99966715eaea0f1e9105c57960eddf6d71c5c9a0
DancingOnAir <taotong1984@hotmail.com>
2022-02-08 16:05:43 +0800

    feat(common): implemented convert corput to one-hot

--- NLP/NaturalLanguageProcessing/common/util.py
+++ NLP/NaturalLanguageProcessing/common/util.py
@@ -102,3 +102,21 @@ def create_contexts_target(corpus, window_size=1):
         contexts.append(cs)
 
     return np.array(contexts), np.array(target)
+
+
+def convert_one_hot(corpus, vocab_size):
+    N = corpus.shape[0]
+
+    if corpus.ndim == 1:
+        one_hot = np.zeros((N, vocab_size), dtype=np.int32)
+        for idx, word_id in enumerate(corpus):
+            one_hot[idx, word_id] = 1
+
+    elif corpus.ndim == 2:
+        C = corpus.shape[1]
+        one_hot = np.zeros((N, C, vocab_size), dtype=np.int32)
+        for idx_0, word_ids in enumerate(corpus):
+            for idx_1, word_id in enumerate(word_ids):
+                one_hot[idx_0, idx_1, word_id] = 1
+
+    return one_hot
--- NLP/NaturalLanguageProcessing/word_distributional_representation/train_simple_cbow.py
+++ NLP/NaturalLanguageProcessing/word_distributional_representation/train_simple_cbow.py
@@ -4,10 +4,23 @@ sys.path.append(os.pardir)
 
 
 from common.trainer import Trainer
+from common.optimizer import Adam
+from common.util import preprocess, create_contexts_target, convert_one_hot
 from simple_cbow import SimpleCBOW
 
 
 def train():
+    window_size = 1
+    hidden_size = 5
+    batch_size = 3
+    max_epoch = 1000
+
+    text = 'You say goodbye and I say hello.'
+    corpus, word_to_id, id_to_word = preprocess(text)
+
+    vocab_size = len(word_to_id)
+    contexts, target = create_contexts_target(corpus, window_size)
+    target = convert_one_hot(target, vocab_size)
     pass
 
 
